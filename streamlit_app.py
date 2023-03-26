9863d0d3df35d2b5b1848f219367312a7403da2b
ayuktha76473 <128321021+ayuktha76473@users.noreply.github.com>
2023-03-27 00:01:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,28 +9,27 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 	
 
 import pandas
-	my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-	my_fruit_list = my_fruit_list.set_index('Fruit')
-	# Let's put a pick list here so they can pick the fruit they want to include
-	fruits_selected= streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-	fruits_to_show= my_fruit_list.loc[fruits_selected]
-	#display the table on the page
-	streamlit.dataframe(fruits_to_show)
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include
+fruits_selected= streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show= my_fruit_list.loc[fruits_selected]
+#display the table on the page
+streamlit.dataframe(fruits_to_show)
 
 
 
-	#import snowflake.connector
-	my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-	my_cur = my_cnx.cursor()
-	my_cur.execute("SELECT * from fruit_load_list")
-	my_data_rows = my_cur.fetchall()
-	streamlit.header("The fruit load list contains:")
-	streamlit.dataframe(my_data_rows)
-	add_my_fruit=streamlit.text_input('What fruit would you like to add?','jackfruit')
-	streamlit.write('Thanks for adding ', add_my_fruit)
+#import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
+add_my_fruit=streamlit.text_input('What fruit would you like to add?','jackfruit')
+streamlit.write('Thanks for adding ', add_my_fruit)
 	
-
-	my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
+my_cur.execute("insert into FRUIT_LOAD_LIST values ('from streamlit')")
 
 
 
