98123e0ff8ae4e2b91f75241f04bd38d75396fe0
ayuktha76473 <128321021+ayuktha76473@users.noreply.github.com>
2023-03-27 01:43:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,8 @@ try:
        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
        #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
        #streamlit.dataframe(fruityvice_normalized)
-#except URLError as e:
-    #streamlit.error()	
+except URLError as e:
+    streamlit.error()	
 	
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
